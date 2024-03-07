a9669e404f18b56310ed489e3492059d80b56852
Pavel Smirnov <striped.page@gmail.com>
2024-03-07 12:46:30 +0300

    feat(fish): enable Vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Cursor shape for Vi mode
+  set fish_cursor_default block
+  set fish_cursor_insert line
+  set fish_cursor_visual block
+  set fish_cursor_replace_one underscore
+
+  # Force Vi cursor, because it not changes in WezTerm
+  set fish_vi_force_cursor
 end
---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,7 @@
+function fish_right_prompt --description 'Write out the right prompt'
+  echo (fish_default_mode_prompt)
+end
+
+# Disable default mode prompt
+function fish_mode_prompt
+end
