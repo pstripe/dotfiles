dff53c2615e8e9e0b9b340c20542ebb95082a9a9
Pavel Smirnov <striped.page@gmail.com>
2024-05-06 17:53:38 +0300

    feat(nvim): add lazygit term bindings

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -144,6 +144,15 @@ later(function()
       height = 50,
     }
   })
+
+  local Terminal  = require('toggleterm.terminal').Terminal
+  local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true })
+
+  function _lazygit_toggle()
+    lazygit:toggle()
+  end
+
+  vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
 end)
 
 later(function()
