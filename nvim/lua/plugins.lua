a3e1ba8975252666a94c87d48eacb33169df32f6
Pavel Smirnov <striped.page@gmail.com>
2024-09-06 13:31:53 +0300

    feat(nvim): add surround plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -159,6 +159,21 @@ require('lazy').setup({
   },
 
   { 'echasnovski/mini.align', version = false, config = true, },
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    opts = {
+      mappings = {
+        add = 'gsa', -- Add surrounding in Normal and Visual modes
+        delete = 'gsd', -- Delete surrounding
+        find = 'gsf', -- Find surrounding (to the right)
+        find_left = 'gsF', -- Find surrounding (to the left)
+        highlight = 'gsh', -- Highlight surrounding
+        replace = 'gsr', -- Replace surrounding
+        update_n_lines = 'gsn', -- Update `n_lines`
+      },
+    },
+  },
 
   {
     'ibhagwan/fzf-lua',
