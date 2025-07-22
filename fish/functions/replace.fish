591ac67b808dacfe4398f4aad09f25b45af32276
Pavel Smirnov <striped.page@gmail.com>
2025-07-22 10:52:35 +0300

    refactor(fish): update replace command

--- fish/functions/replace.fish
+++ fish/functions/replace.fish
@@ -1,3 +1,8 @@
-function replace
-  sd "$argv[1]" "$argv[2]" (rg "$argv[1]" "$argv[3]" --hidden --files-with-matches)
+function replace --wraps='sd $1 $2 (rg $1 --files-with-matches)'
+  set --local orig $argv[1]
+  set --local new $argv[2]
+
+  set --erase argv[1] argv[1]
+  
+  sd "$orig" "$new" "$argv" (rg "$orig" --files-with-matches "$argv")
 end
