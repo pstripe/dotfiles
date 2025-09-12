aea47adc1f73c9f43c1394c2a3c683ee464620a4
Pavel Smirnov <striped.page@gmail.com>
2025-09-12 12:18:14 +0300

    refactor(fish): split configuration

--- fish/conf.d/01-env.fish
+++ fish/conf.d/01-env.fish
@@ -2,3 +2,7 @@ set --universal --export LANG en_US.UTF-8
 set --universal --export XDG_CONFIG_HOME "$HOME/.config"
 set --universal --export MANPAGER 'nvim +Man!'
 set --universal --export EDITOR hx
+
+if status is-interactive
+    set __fish_ls_command eza
+end
--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,2 @@
 if status is-interactive
-  # Cursor shape for Vi mode
-  set fish_cursor_default block
-  set fish_cursor_insert line
-  set fish_cursor_visual block
-  set fish_cursor_replace_one underscore
-
-  # Force Vi cursor, because it not changes in WezTerm
-  set fish_vi_force_cursor
-
-  set __fish_ls_command eza
 end
