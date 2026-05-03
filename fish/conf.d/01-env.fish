c7cbb9d6e978fbe8600f561155ea769b3a31b40f
Pavel Smirnov <striped.page@gmail.com>
2026-05-03 16:14:40 +0300

    feat(fish)!: replace man pager with bat

--- fish/conf.d/01-env.fish
+++ fish/conf.d/01-env.fish
@@ -1,7 +1,10 @@
 set --global --export LANG en_US.UTF-8
 set --global --export XDG_CONFIG_HOME "$HOME/.config"
-set --global --export MANPAGER 'nvim +Man!'
 set --global --export EDITOR hx
+
+# man
+set --global --export MANPAGER "fish -c 'col -bx | bat -plman'"
+# set --global --export MANROFFOPT '-c'
 
 if status is-interactive
     set __fish_ls_command eza
